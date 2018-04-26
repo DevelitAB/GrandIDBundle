8197ae280b0310d02070057c94a60b5a000fabd6
Stas Bannikov <sab.ukraine@gmail.com>
2018-04-26 09:41:51 +0300

    GrandID service updated

--- Services/GrandID.php
+++ Services/GrandID.php
@@ -2,12 +2,37 @@
 
 namespace Bsadnu\GrandIDBundle\Services;
 
+use GuzzleHttp\Client as HttpClient;
+
 class GrandID
 {
+    const ERR_OBJ_CODES = [
+            'APIKEYNOTVALID01',
+            'FIELDSNOTVALID',
+        ];
+
     /**
      * @var array
      */
     private $config;
+
+    /** @var HttpClient */
+    private $httpClient;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * @var string
+     */
+    private $authenticateServiceKey;
 
     /**
      * SlackBot constructor.
@@ -17,6 +42,12 @@ class GrandID
     public function __construct(array $config)
     {
         $this->setConfig($config);
+
+        $this->httpClient = new HttpClient();
+
+        $this->baseUrl = $this->config['base_url'];
+        $this->apiKey = $this->config['api_key'];
+        $this->authenticateServiceKey = $this->config['authenticate_service_key'];
     }
 
     /**
@@ -35,8 +66,46 @@ class GrandID
         $this->config = $config;
     }
 
-    public function someFunc()
+    public function federatedLogin($callbackUrl)
     {
-        return $this->config['base_url'];
+        $uri = $this->baseUrl . 'FederatedLogin?authenticateServiceKey=' . $this->authenticateServiceKey . '&apiKey=' . $this->apiKey . '&callbackUrl=' . $callbackUrl;
+
+        $response = $this->httpClient->request('GET', $uri);
+
+        return json_decode($response->getBody());
+    }
+
+    public function federatedDirectLogin($username, $password)
+    {
+        $uri = $this->baseUrl . 'FederatedDirectLogin?authenticateServiceKey=' . $this->authenticateServiceKey . '&apiKey=' . $this->apiKey . '&username=' . $username . '&password=' . $password;
+
+        $response = $this->httpClient->request('GET', $uri);
+
+        return json_decode($response->getBody());
+    }
+
+    public function logout($sessionId)
+    {
+        $uri = $this->baseUrl . 'Logout?authenticateServiceKey=' . $this->authenticateServiceKey . '&apiKey=' . $this->apiKey . '&sessionid=' . $sessionId;
+
+        $response = $this->httpClient->request('GET', $uri);
+
+        return json_decode($response->getBody());
+    }
+
+    public function getSession($sessionId)
+    {
+        $uri = $this->baseUrl . 'GetSession?authenticateServiceKey=' . $this->authenticateServiceKey . '&apiKey=' . $this->apiKey . '&sessionid=' . $sessionId;
+
+        $response = $this->httpClient->request('GET', $uri);
+
+        return json_decode($response->getBody());
+    }
+
+    public function isLoginSuccessful($sessionId)
+    {
+        $sessionObject = $this->getSession($sessionId);
+
+        return property_exists($sessionObject, 'username');
     }
 }
