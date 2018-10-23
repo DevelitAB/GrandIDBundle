474740db810cf42999824a2bebd5da7a8dd934c8
Stas Bannikov <sab.ukraine@gmail.com>
2018-10-23 16:52:14 +0300

    Update GrandIDBundle.php

--- GrandIDBundle.php
+++ GrandIDBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Bsadnu\GrandIDBundle;
+namespace DevelitAB\GrandIDBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
